088342fdce9e094e004de5a8b452f992ab6a6936
Nicolas <nicolas.went@epita.fr>
2023-09-29 11:05:57 +0200

    [GetPairs] WIP on getting the pairs

--- src/api/get_pairs.rs
+++ src/api/get_pairs.rs
@@ -3,21 +3,33 @@ use eyre::Result;
 use super::models::Pairs;
 
 async fn get_pair(api_url: &String, api_key: &String, page: u32) -> Result<Pairs> {
-    todo!("Not implemented")
+    let client = reqwest::Client::new();
+    let response: Pairs = client.get(format!("{api_url}/v0/exchanges/pairs/?page={page}&blockchain=ethereum&exchange=uniswapv2_ethereum"))
+        .header("api-key", api_key)
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    Ok(response)
 }
 
 /// Get the pairs using Blockchain APIs
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `api_url` - The url of Blockchain APIs
 /// * `api_key` - The key that we use
 /// * `pair_amount` - The amount of pairs that we have to get
-/// 
+///
 /// # Returns
-/// 
+///
 /// A vector containing the pairs that we got
-pub async fn get_pairs(api_url: &String, api_key: &String, pair_amount: u64) -> Result<Vec<(String, String)>> {
+pub async fn get_pairs(
+    api_url: &String,
+    api_key: &String,
+    pair_amount: u64,
+) -> Result<Vec<(String, String)>> {
     let mut pairs = Vec::new();
     let mut curr_amount = 0;
     let mut page = 1_u32;
